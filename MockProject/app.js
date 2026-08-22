e5fc216ce2eb0bf98b26bbbf1f7aea6f2bf7b1bf
longbugi <longpd19@gmail.com>
2026-08-22 09:41:34 +0000

    Add source code and configs to MockProject

---
+++ MockProject/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Deploy thành công lên AWS EKS Cluster!</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`Application running on port ${port}`);
+});
