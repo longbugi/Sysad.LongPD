d1aa3061e64915c5fba7a0a718f2df36a95c02b1
longbugi <longpd19@gmail.com>
2026-08-22 15:08:43 +0000

    Test webhook

--- MockProject/app.js
+++ MockProject/app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("<h1>Deploy thành công lên AWS EKS Cluster!</h1>");
+  res.send("<h1>Long Pham - webhook test!</h1>");
 });
 
 app.listen(port, () => {
